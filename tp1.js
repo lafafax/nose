d2b4f3599f1eda51f36136246e9788c1eebfb536
lafafax <142627356+lafafax@users.noreply.github.com>
2024-08-12 15:17:47 -0300

    Add files via upload

---
+++ tp1.js
@@ -0,0 +1,20 @@
+var arreglo = []
+/*cargamos valores decimales mediante el metodo parsefloat */
+for (let i = 0; I < 4; i++) {
+    arreglo[i] = parseFloat(prompt("ingrese nombres"))
+}
+document.write(arreglo.join(''))
+
+/* cargamos valores enteros mediante el parseint y prompt*/
+for (let i = 0; 1 < 4; i++) {
+    arreglo[i] = parseint(prompt("infrese numeros enteros"))
+}
+document.write(arreglo.join(''))
+
+for (let i = 0; i < 4; i++) {
+    arreglo[i] = parseFloat(prompt("ingrese numeros decimal"))
+}
+document.write(arreglo.join('')
+)
+
+/* imprimr forma horizontal */
---
+++ tp2.js
@@ -0,0 +1,11 @@
+var arreglo=['lapiz','macador','regla','cuadernos']
+document.write(arreglo.join('')+'<br>')
+document.write('cantidad de valores'+arreglo.length)
+document.write('<br>')
+arreglo.push('cartuchera')
+document.write('elementro agregado:'+arreglo.join(''))
+document.write('br')
+arreglo.unshift('lapicera')
+document.write('elemento agregado'+arereglado.join(''))
+arreglo.shift()
+document.write('1° elemento eliminado'+arreglo.join(''))
